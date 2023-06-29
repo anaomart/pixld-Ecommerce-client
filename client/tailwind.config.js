4fccdae764a2b25dd21a263152faf3f968998ae2
anaomart <anaomart@gmail.com>
2023-06-29 22:46:56 +0300

    fix bg url

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,24 +2,24 @@
 import daisyui from "daisyui";
 
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            backgroundImage: {
-                hero: `url(./src/assets/Hero.png)`,
-                NewArrivals: "url(./src/assets/newArrivles.jpg)",
-                discount: "url(./src/assets/discounts.jpeg)",
-            },
-            colors: {
-                dark: {
-                    100: "#1E293B",
-                },
-                secondary: "#64748B",
-            },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        hero: "url('./src/assets/Hero.png')",
+        NewArrivals: "url('./src/assets/newArrivles.jpg')",
+        discount: "url('./src/assets/discounts.jpeg')",
+      },
+      colors: {
+        dark: {
+          100: "#1E293B",
         },
+        secondary: "#64748B",
+      },
     },
-    plugins: [daisyui],
-    daisyui: {
-        themes: ["light", "dark", "cupcake"],
-    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 };
